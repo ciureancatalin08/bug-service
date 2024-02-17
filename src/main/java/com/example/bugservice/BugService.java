c850ab6096ba843e4fe9718bc5ab2f5103341adb
cataciurean@rocketmail.com <cataciurean@rocketmail.com>
2024-02-17 16:07:06 +0200

    kubernetes deployment

--- src/main/java/com/example/bugservice/BugService.java
+++ src/main/java/com/example/bugservice/BugService.java
@@ -2,12 +2,15 @@ package com.example.bugservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.Level;
 
 @SpringBootApplication
 public class BugService {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(BugService.class, args);
-	}
+    public static void main(String[] args) {
+        ch.qos.logback.classic.Logger hikariLogger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("com.zaxxer.hikari");
+        hikariLogger.setLevel(Level.ALL);
+        SpringApplication.run(BugService.class, args);
+    }
 }
--- src/main/java/com/example/bugservice/persistence/entity/User.java
+++ src/main/java/com/example/bugservice/persistence/entity/User.java
@@ -4,7 +4,6 @@ package com.example.bugservice.persistence.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.ArrayList;
 import java.util.List;
